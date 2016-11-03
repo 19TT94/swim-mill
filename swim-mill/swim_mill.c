d6d73e8fcb2b9f774f1a8a2708af057a17e42f5b
Taylor Tobin <19tt94@gmail.com>
2016-11-02 19:34:11 -0700

    setting up river

--- swim-mill/swim_mill.c
+++ swim-mill/swim_mill.c
@@ -8,8 +8,42 @@
 
 #include <stdio.h>
 
+const char water = '~';
+const int river_length = 10;
+const int river_width = 10;
+
+// 2-D array to simulate river
+char (*river)[river_length][river_width];
+
+void genRiver();
+void printRiver();
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    //Generate River
+    genRiver();
+    
+    printf("hi");
+    printRiver();
+    
     return 0;
+}
+
+// function that populates the river array with water
+void genRiver() {
+    for(int i=0; i < river_length; i++) {
+        printf("test");
+        for(int j=0; j < river_width; j++ ) {
+            (*river)[i][j] = water;
+        }
+    }
+}
+
+// function that prints river array
+void printRiver() {
+    for(int i=0; i < river_length; i++) {
+        for(int j=0; j < river_width; j++ ) {
+            printf("%c", (*river)[i][j]);
+        }
+        printf("/n");
+    }
 }
